49273936100718e0c814b5c40499ade45d335a3a
tobynorth <toby.ross.north@gmail.com>
2021-12-19 19:15:45 -0500

    Install @react-three/drei in order to add easy rotation/zoom

--- components/galaxy-map.tsx
+++ components/galaxy-map.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import CelestialBody from './celestial-body';
+import { OrbitControls } from '@react-three/drei'
 
 export default function GalaxyMap() {
   return (
-    <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <CelestialBody position={[-1.2, 0, 0]} />
-        <CelestialBody position={[1.2, 0, 0]} />
-    </Canvas>
+      <Canvas camera={{fov: 10, position: [0, 0, -100]}}>
+          <OrbitControls enablePan={false} enableZoom={true} enableRotate={true} />
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <CelestialBody position={[-10.2, 2, -30]} />
+          <CelestialBody position={[0, 0, 0]} />
+      </Canvas>
   )
 }
