defaade296a66ef5f07f5de66d20e73e6b8ea873
tobynorth <toby.ross.north@gmail.com>
2021-12-22 20:29:29 -0500

    Define props for celestial bodies, pass in spherical coords and convert them to Cartesian coords (unfortunately three.js's Vector3.setFromSphericalCoordinates method doesn't currently seem to be supported in @react-three/fiber)

--- components/celestial-body.tsx
+++ components/celestial-body.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
 
-export default function CelestialBody(props: JSX.IntrinsicElements['mesh']) {
+interface CelestialBodyProps {
+  distance: number,
+  declination: number,
+  rightAscension : number
+};
+
+export default function CelestialBody(props: CelestialBodyProps) : JSX.Element {
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
+  let x = props.distance * Math.cos(props.declination) * Math.cos(props.rightAscension),
+    y = props.distance * Math.cos(props.declination) * Math.sin(props.rightAscension),
+    z = props.distance * Math.sin(props.declination);
   return (
     <mesh
-      {...props}
+      position={[x, y, z]}
       scale={clicked ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
--- components/galaxy-map.tsx
+++ components/galaxy-map.tsx
@@ -4,13 +4,29 @@ import CelestialBody from './celestial-body';
 import { OrbitControls } from '@react-three/drei'
 
 export default function GalaxyMap() {
+  // Temporary test data
+  let celestialBodies = [
+    // The Sun
+    {
+      name: "Sun",
+      distance: 0,
+      declination: 0,
+      rightAscension: 0
+    },
+    // Aldebaran (data from Wikipedia)
+    {
+      name: "Aldebaran",
+      distance: 65.3,
+      declination: (16 + 30/60 + 33.4885/3600) * Math.PI / 180,
+      rightAscension: (15 * (4 + 35/60 + 55.23907/3600)) * Math.PI / 180
+    }
+  ];
   return (
       <Canvas camera={{fov: 10, position: [0, 0, -100]}}>
           <OrbitControls enablePan={false} enableZoom={true} enableRotate={true} />
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <CelestialBody position={[-10.2, 2, -30]} />
-          <CelestialBody position={[0, 0, 0]} />
+          { celestialBodies.map(c => <CelestialBody {...c} key={c.name} />) }
       </Canvas>
   )
 }
