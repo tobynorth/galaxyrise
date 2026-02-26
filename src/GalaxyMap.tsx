2198385249df7e08fdd596d1baa9552b76f52993
tobynorth <toby.ross.north@gmail.com>
2026-02-25 22:55:30 -0500

    Remove unused code from GalaxyMap.tsx

--- src/GalaxyMap.tsx
+++ src/GalaxyMap.tsx
@@ -5,15 +5,9 @@ import { OrbitControls } from '@react-three/drei'
 import { Vector3 } from 'three'
 import data from './celestialBodies.json'
 
-const LY_PER_UNIT : number = 3
-const MAX_HORIZ_UNITS_FROM_ORIGIN : number = 10
-const MAX_VERT_UNITS_FROM_ORIGIN : number = 5
-const Y_OFFSET: number = 0.5
-
 let convertToGridCoords = (coords: number[]) : Vector3 => {
   let v = new Vector3(...coords);
   v.setFromSphericalCoords(v.x < 1 ? 0 : Math.log2(v.x), (-1 * v.y + 90) * Math.PI / 180, (v.z + 90) * Math.PI / 180);
-  v.multiplyScalar(1 / LY_PER_UNIT);
   return v;
 }
 
@@ -45,14 +39,7 @@ export default function GalaxyMap() {
           <color attach="background" args={["black"]} />
           <HexagonalGrid size={2} circleCount={7} />
           { celestialBodies
-            .filter(c =>
-              c.enabled &&
-              c.coordinates.x >= -MAX_HORIZ_UNITS_FROM_ORIGIN &&
-              c.coordinates.x <= MAX_HORIZ_UNITS_FROM_ORIGIN &&
-              c.coordinates.y >= Y_OFFSET - MAX_VERT_UNITS_FROM_ORIGIN &&
-              c.coordinates.y <= MAX_VERT_UNITS_FROM_ORIGIN + Y_OFFSET &&
-              c.coordinates.z >= -MAX_HORIZ_UNITS_FROM_ORIGIN &&
-              c.coordinates.z <= MAX_HORIZ_UNITS_FROM_ORIGIN)
+            .filter(c => c.enabled)
             .map(c => <CelestialBody {...c} key={c.name} />) }
       </Canvas>
   )
