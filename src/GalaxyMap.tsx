92966ce15d49ea8cdf812c2803941a1caa2dba68
tobynorth <toby.ross.north@gmail.com>
2026-02-26 00:13:04 -0500

    Do a bunch of cleanup

--- src/GalaxyMap.tsx
+++ src/GalaxyMap.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { CelestialBody, CelestialBodyProps } from './CelestialBody';
 import { OrbitControls } from '@react-three/drei'
